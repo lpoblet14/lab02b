f9586fedb5d83d4fcf81f803e63913c8248364f4
eaperez <eaperez@alumnos.ubiobio.cl>
2014-04-09 19:49:37 -0300

    label

--- lab3/src/lab3/ventana.java
+++ lab3/src/lab3/ventana.java
@@ -1,8 +1,12 @@
 package lab3;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 public class ventana {
 	public static void main (String[] args){
 		JFrame frame = new JFrame();
+		JLabel label = new JLabel();
+		label.setText("Hola soy un Label");
+		frame.add(label);
 		frame.setTitle("Mi primera ventana");
 		frame.setSize(800,600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
