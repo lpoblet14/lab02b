8949d9cb10c2df562faf61a31c4b7b5251648207
eaperez <eaperez@alumnos.ubiobio.cl>
2014-04-09 19:57:41 -0300

    button

--- lab3/src/lab3/ventana.java
+++ lab3/src/lab3/ventana.java
@@ -1,4 +1,5 @@
 package lab3;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 public class ventana {
@@ -7,6 +8,9 @@ public class ventana {
 		JLabel label = new JLabel();
 		label.setText("Hola soy un Label");
 		frame.add(label);
+		JButton button = new JButton();
+		button.setText("presioname");
+		frame.add(button);
 		frame.setTitle("Mi primera ventana");
 		frame.setSize(800,600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
