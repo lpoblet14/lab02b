ca0e81eb14bf0cc55e2ec74328cb5643a3f42172
lpoblet14 <l.poblete.f@gmail.com>
2014-04-09 19:37:09 -0300

    creacion ventana

---
+++ lab3/src/lab3/ventana.java
@@ -0,0 +1,12 @@
+package lab3;
+import javax.swing.JFrame;
+public class ventana {
+	public static void main (String[] args){
+		JFrame frame = new JFrame();
+		frame.setTitle("Mi primera ventana");
+		frame.setSize(800,600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+	}
+}
